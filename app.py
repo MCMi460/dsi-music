120306436c653536423c46d4383de12a2b9372f3
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2024-07-18 22:08:03 -0500

    Add downloading via `yt_dlp`

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import music
 
+def ask(query:str):
+    answers = [
+        i.split(')')[0].lower()
+        for i in query.split('(')
+        if ')' in i
+    ]
+    return answers.index(input(query).lower())
+
 if __name__ == '__main__':
-    pass
+    match ask('(S)potify or (A)pple Music?\n> '):
+        case 0:
+            provider = music.sfy
+        case 1:
+            provider = music.apm
+    playlist = provider.playlist(input('Playlist link\n> '))
+    print('Playlist indexed. Backing playlist via Youtube Music.')
+    music.ytm.playlist(playlist)
--- music/ytm.py
+++ music/ytm.py
@@ -1,16 +1,39 @@
 from ytmusicapi import YTMusic
 from .models import Track, Playlist
+from yt_dlp import YoutubeDL
+from os.path import exists
+from os import makedirs
 
 yt = YTMusic()
 
+opts = {
+    'format': 'bestaudio[ext=m4a]/best',
+    'postprocessors': [
+        {
+            'key': 'FFmpegMetadata',
+            'add_metadata': True
+        }
+    ]
+}
+
 class ytm:
-    def track(track:Track):
+    def track(track:Track, *, folder:str = 'export') -> str:
         search_results = yt.search(
             ' '.join([track.name, track.artist, track.album]),
-            filter = 'songs'
+            filter = 'songs',
+            limit = 1
         )
-        return search_results
+        id = search_results[0]['videoId']
+
+        if not exists(folder):
+            makedirs(folder)
+        opts['outtmpl'] = f'{folder}/%(title)s.%(ext)s'
+
+        with YoutubeDL(opts) as ydl:
+            ydl.download(id)
+
+        return id
 
     def playlist(playlist:Playlist):
         for track in playlist.tracks:
-            print(ytm.track(track))
+            ytm.track(track, folder = f'export/{playlist.name}')
